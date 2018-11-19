e98974d24e87f50e4109c353725ddc1559624130
Michael <freefaller@gmail.com>
2018-11-19 09:31:53 -0800

    Styling and routing changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PostListComponent },
+  { path: '**', component: PostListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
