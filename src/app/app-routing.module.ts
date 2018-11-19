9a85efe7919d9be0f6fc0aef8ef9bd19079f018b
Michael <freefaller@gmail.com>
2018-11-19 14:35:18 -0800

    Layout fixes and duplicate posts fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
+import { PostsComponent } from './posts/posts.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: PostListComponent },
-  { path: '**', component: PostListComponent }
+  { path: '', component: PostsComponent },
+  { path: '**', component: PostsComponent }
 ];
 
 @NgModule({
