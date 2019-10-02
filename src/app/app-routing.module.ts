35480550414f7bd1a849343c2a4c0ef955f0ddab
Michael <freefaller@gmail.com>
2019-10-01 20:23:55 -0400

    update routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,19 @@ import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { PostsComponent } from './posts/posts.component';
+import { PostsResolver } from './posts/posts.resolver';
 
 const routes: Routes = [
   { path: '', component: LoginComponent, pathMatch: 'full' },
   { path: 'login' , component: LoginComponent },
   { path: 'signup' , component: SignupComponent },
-  { path: 'posts', component: PostsComponent, canActivate: [AuthGuard] },
+  { path: 'posts',
+    component: PostsComponent,
+    resolve: {
+      posts: PostsResolver
+    },
+    canActivate: [AuthGuard]
+  },
   { path: '**', component: LoginComponent }
 ];
 
